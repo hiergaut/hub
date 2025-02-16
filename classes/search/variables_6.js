299cb008a51f21e5320d2ad61e5fcfb2ef957986
hiergaut <gauthierbouyjou@aol.com>
2025-02-16 18:43:26 +0000

    Deploying to gh-pages from @ hiergaut/hub@2bc15ef070e64bca384a59ddc5dcc15f18041eda 🚀

---
+++ classes/search/variables_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['g_0',['g',['../structhub_1_1format_1_1RGB8.html#a22618caa3f21c190b19b6a4a90c45a98',1,'hub::format::RGB8::g'],['../structhub_1_1format_1_1RGBA8.html#a0149eefba09f564ec1dc3a34c49d819e',1,'hub::format::RGBA8::g'],['../structhub_1_1format_1_1BGR8.html#ab0551afe59a3783088c18f3584fd936b',1,'hub::format::BGR8::g']]],
+  ['get_1',['get',['../classhub_1_1MatrixXDBase.html#aff8389dd7b7d76aa986baba751d0c239',1,'hub::MatrixXDBase']]]
+];
---
+++ classes/search/variables_f.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['s_5fbufferacqsize_0',['s_bufferAcqSize',['../classhub_1_1io_1_1StreamMqtt.html#af4914223420108db3c9848fd8394a22a',1,'hub::io::StreamMqtt']]],
+  ['s_5fcommithash_1',['s_commitHash',['../namespacehub.html#ad62e4203964641ac969953db85a08399',1,'hub']]],
+  ['s_5fdefaultipv4_2',['s_defaultIpv4',['../classhub_1_1io_1_1StreamMqtt.html#af5138671ecb68a9dd17e062b9b2e1529',1,'hub::io::StreamMqtt']]],
+  ['s_5fdefaultport_3',['s_defaultPort',['../classhub_1_1io_1_1StreamMqtt.html#ae30ee36a958f1d75d8f0121a79f79d3e',1,'hub::io::StreamMqtt']]],
+  ['s_5ftopicevents_4',['s_topicEvents',['../classhub_1_1io_1_1StreamMqtt.html#a977e5c8423e00e08a35357ff2128802b',1,'hub::io::StreamMqtt']]],
+  ['s_5ftopicstream_5',['s_topicStream',['../classhub_1_1io_1_1StreamMqtt.html#a41b4391d8136eb965fa8792afaed5e37',1,'hub::io::StreamMqtt']]],
+  ['s_5ftopicviewer_6',['s_topicViewer',['../classhub_1_1io_1_1StreamMqtt.html#ad06c3ce31d1faf41890b2bef034e90e2',1,'hub::io::StreamMqtt']]],
+  ['s_5fversion_7',['s_version',['../namespacehub.html#aae13dc318cbfaa4d81c37c1ce1b44736',1,'hub']]],
+  ['s_5fversionmajor_8',['s_versionMajor',['../namespacehub.html#af12f6b550dd9c4d7bcb2c8b9c5f70771',1,'hub']]],
+  ['s_5fversionminor_9',['s_versionMinor',['../namespacehub.html#a2d69e7b948116c3e916040294d82ab23',1,'hub']]],
+  ['s_5fversionpatch_10',['s_versionPatch',['../namespacehub.html#a71bc320eb370da3c59bfaa75f6d4320f',1,'hub']]],
+  ['serializables_11',['Serializables',['../classhub_1_1SerializerT.html#a0cd838c2030d38f446dabe4c5defdf92',1,'hub::SerializerT']]],
+  ['side_12',['side',['../classhub_1_1sensor_1_1outputSensor_1_1OutputSensor__3D__Density.html#ae5ac6dd1394bde951a94850264b65a9f',1,'hub::sensor::outputSensor::OutputSensor_3D_Density']]],
+  ['size_13',['Size',['../classhub_1_1MatrixTs.html#a28a4f778073cd502f1bcbbeb60c0acd0',1,'hub::MatrixTs::Size'],['../classhub_1_1MatrixXDBase.html#a8f764b87f5bf6a36dc3a91282444ae40',1,'hub::MatrixXDBase::Size']]]
+];
