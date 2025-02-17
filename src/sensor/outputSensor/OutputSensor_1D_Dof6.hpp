52f38bf911fdd17f4e5ac8ea87e4ce0abf569758
Gauthier Bouyjou <gauthierbouyjou@aol.com>
2025-02-17 23:26:45 +0100

    update example 6DOF

--- src/sensor/outputSensor/OutputSensor_1D_Dof6.hpp
+++ src/sensor/outputSensor/OutputSensor_1D_Dof6.hpp
@@ -16,6 +16,37 @@
 
 #pragma once
 
+#include <cmath>
+
+// source: https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+void EulerToQuaternion( double roll,
+                        double pitch,
+                        double yaw,
+                        double& w0,
+                        double& w1,
+                        double& w2,
+                        double& w3 ) // roll (x), pitch (y), yaw (z), angles are in radians
+{
+    // Abbreviations for the various angular functions
+
+    constexpr auto pi = 3.1415;
+    roll *= pi / 180;
+    pitch *= pi / 180;
+    yaw *= pi / 180;
+
+    double cr = cos( roll * 0.5 );
+    double sr = sin( roll * 0.5 );
+    double cp = cos( pitch * 0.5 );
+    double sp = sin( pitch * 0.5 );
+    double cy = cos( yaw * 0.5 );
+    double sy = sin( yaw * 0.5 );
+
+    w0 = cr * cp * cy + sr * sp * sy;
+    w1 = sr * cp * cy - cr * sp * sy;
+    w2 = cr * sp * cy + sr * cp * sy;
+    w3 = cr * cp * sy - sr * sp * cy;
+}
+
 #include "OutputSensor_Instance.hpp"
 
 namespace hub {
@@ -70,14 +101,38 @@ class OutputSensor_1D_Dof6 : public OutputSensor_Instance
 
         int dec = 0;
 
+        double roll  = 0.0;
+        double pitch = 0.0;
+        double yaw   = 0.0;
+        double w0, w1, w2, w3;
+
         while ( m_running ) {
             const auto startClock = std::chrono::high_resolution_clock::now();
 
-            start  = hub::sensor::getClock();
-            dof6.x = dec % 100;
-            dof6.y = dec % 100;
-            dof6.z = dec % 100;
-            end    = hub::sensor::getClock();
+            // EulerToQuaternion(0.0, 0.0, (dec / 5) % 360, w0, w1, w2, w3);
+            // EulerToQuaternion(0.0, 0.0, 0, w0, w1, w2, w3); // south
+            // EulerToQuaternion(0.0, 0.0, 90.0, w0, w1, w2, w3); // east
+            // EulerToQuaternion(0.0, 0.0, -90.0, w0, w1, w2, w3); // west
+            // EulerToQuaternion(0.0, 0.0, 180.0, w0, w1, w2, w3); // north
+
+            // EulerToQuaternion( 0.0, ( dec / 10 ) % 360, 0.0, w0, w1, w2, w3 );
+            // EulerToQuaternion(0.0, 0.0, 0.0, w0, w1, w2, w3); // +20
+            // EulerToQuaternion(0.0, 20.0, 0.0, w0, w1, w2, w3); // +20
+            // EulerToQuaternion(0.0, 0.0, 90.0, w0, w1, w2, w3); // east
+            // EulerToQuaternion(0.0, 0.0, -90.0, w0, w1, w2, w3); // west
+            // EulerToQuaternion(0.0, 0.0, 180.0, w0, w1, w2, w3); // north
+
+            EulerToQuaternion( 0.0, ( dec / 10 ) % 360, (dec / 5) % 360, w0, w1, w2, w3 );
+
+            start = hub::sensor::getClock();
+            // dof6.x = ( dec % 100 ) / 100.0;
+            // dof6.y = ( dec % 100 ) / 100.0;
+            // dof6.z = ( dec % 100 ) / 100.0;
+            dof6.w0 = w0;
+            dof6.w1 = w1;
+            dof6.w2 = w2;
+            dof6.w3 = w3;
+            end     = hub::sensor::getClock();
             ++dec;
 
             if ( onNewAcq ) { onNewAcq( m_streamName, acq ); }
