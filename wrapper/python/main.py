5a241422b7fb3743c7188506517b3accc993828a
hiergaut <gauthierbouyjou@aol.com>
2025-03-07 14:34:57 +0100

    cleanup python example

--- wrapper/python/main.py
+++ wrapper/python/main.py
@@ -1,19 +1,12 @@
-#import sys
-import time
-#import ctypes
 
+import time
 import hubPy
 
-print("hub wrapper python worked")
-
-
-from hubPy import *
+# from hubPy import *
 from time import sleep
 
 
-# hubPy
 outputSensor = hubPy.OutputSensor('pySensor', 'pyStream')
-# outputSensor.helloWorld()
 
 for i in range(0, 10):
     print(i)
@@ -28,80 +21,3 @@ for i in range(0, 10):
     outputSensor.w3 = i
     outputSensor.newAcq()
     sleep(0.1)
-
-# outputSensor.acq = outputSensor.Acq(
-#     # imufusion.CONVENTION_NWU,  # convention
-#     0,
-#     0.5,  # gain
-#     2000,  # gyroscope range
-#     10,  # acceleration rejection
-#     10,  # magnetic rejection
-#     5  # recovery trigger period = 5 seconds
-# )
-
-
-
-# import numpy as np
-# from matplotlib import pyplot as plt
-
-# width = 512
-# depth = 512
-# height = 100
-
-# resolution = [width, depth, height], Format.DENSITY
-# print(resolution)
-
-# metadata = { "realWidth" : 100.0, "realDepth" : 100.0, "realHeight" : 50.0 }
-# print(metadata)
-
-# sensorSpec = SensorSpec("sensorName", [resolution], metadata)
-# print(sensorSpec.to_string())
-
-
-# streamer = Streamer("127.0.0.1", 4042)
-# streamer.addStream("streamName", sensorSpec)
-
-
-# data = np.zeros((width, depth, height, 4), dtype=np.uint8)
-# #data[0:width,0:height] = [255, 0, 0]
-# #data = np.zeros((width, depth, height, 4))
-# data[0:width,0:depth,0:height,0:4] = 127
-# print(data.shape)
-
-
-# vector = data.flatten()
-# print(vector.shape)
-
-# acq = Acquisition(1, 1)
-
-# measure = Measure(vector, resolution)
-
-# acq.addMeasure(measure)
-
-# streamer.newAcq("streamName", acq)
-
-
-# time.sleep(1)
-# iAcq = 0
-
-# while streamer.isConnected():
-
-#     vector = data.flatten()
-
-#     acq = Acquisition(1, 1)
-
-#     measure = Measure(vector, resolution)
-
-#     acq.addMeasure(measure)
-
-#     streamer.newAcq("streamName", acq)
-
-#     print("sended acq " + str(iAcq))
-
-#     try:
-#         time.sleep(1)
-#     except KeyboardInterrupt:
-#         print("The program is terminated manually!")
-#         raise SystemExit
-
-#     iAcq += 1
